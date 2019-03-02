ruimendes29/dotfiles

Refactor installation process
This is a big change on how it was working before. I am trying this way
to see what I can do.

.setup/distro.sh
@@ -1,0 +1,8 @@
+#!/bin/bash
+
+export PKGMN=pacman # Package Manager
+export PKGOPT=(--needed --noconfirm)
+export PKGU=-Syuq # Updating Options
+export PKGI=-Syq  # Installation Options
+export PKGR=-Rns # Remove Options
+

fossabot/dotfiles-18

distro.sh
@@ -1,0 +1,7 @@
+#!/bin/bash
+
+export PKGMN=pacman # Package Manager
+export PKGOPT=(--needed --noconfirm)
+export PKGU=-Syu # Updating Options
+export PKGI=-Sy  # Installation Options
+export PKGR=-Rns # Remove Options